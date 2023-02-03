a6b9820b51d534c1d8309b43a31d1c19d997ae1d
Zhenya20062 <matveeveech1986@yandex.ru>
2023-02-03 10:31:51 +0300

    use enum convention

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,26 +1,29 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 public class Card
 {
-    public Sprite image;
-    public int value{get;private set;}
-    
-    public Card(int value, Sprite image) {
-        CARD card = (CARD)value;
-        this.value = (int)Enum.Parse(typeof(VALUECARD), card.ToString());
+    public Sprite image { get; private set; }
+    public int value { get; private set; }
+
+    public Card(int value, Sprite image)
+    {
+        CardName cardName = (CardName)value;
+        this.value = (int)Enum.Parse(typeof(CardValue), cardName.ToString());
         this.image = image;
     }
 
-    public enum VALUECARD
+    public enum CardValue
     {
-        JACKET = 2, LADY = 3, KING = 4, ACE = 11, SIX = 6, 
+        JACKET = 2, LADY = 3, KING = 4, ACE = 11, SIX = 6,
         SEVEN = 7, EIGHT = 8, NINE = 9, TEN = 10
     }
-    public enum CARD
+
+    //Names of enumeration types (also called enums) in general should follow the standard type-naming rules (PascalCasing, etc.)
+    //source:https://learn.microsoft.com/en-us/dotnet/standard/design-guidelines/names-of-classes-structs-and-interfaces#naming-enumerations
+    public enum CardName
     {
         JACKET, LADY, KING, ACE, SIX,
         SEVEN, EIGHT, NINE, TEN
